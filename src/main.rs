c6f6280044dc22b6961501ee9a65a475fbe09379
Gregor Tudan <gregor.tudan@cofinpro.de>
2018-02-18 16:18:29 +0100

    moved readme

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,25 @@ use std::error::Error;
 use hid::Manager;
 use std::time::Duration;
 
-
-const HID_SETREPORT : u32 = 0xc0094806;
+const HID_SETREPORT: u32 = 0xc0094806;
 
 fn main() {
-
     initialize();
 }
 
 fn initialize() {
     let manager = Manager;
-    let mut devices = manager.find(Some(0x04d9),Some(0xa052));
+    let mut devices = manager.find(Some(0x04d9), Some(0xa052));
     let device = match devices.next() {
         Some(device) => device,
-        None         => panic!("Device not found!"),
+        None => panic!("Device not found!"),
     };
 
     println!("Found device at address {}", device.path().display());
 
     let mut handle = match device.open() {
         Ok(handle) => handle,
-        Err(why)   => panic!("Failed to open device {}", why.description()),
+        Err(why) => panic!("Failed to open device {}", why.description()),
     };
 
     let report_id: u8 = 0x1;
@@ -35,10 +33,8 @@ fn initialize() {
     loop {
         match handle.data().read(&mut data, Duration::from_secs(5)) {
             Ok(length) => println!("Read {} bytes", length.expect("No bytes read!")),
-            Err(why)   => println!("{}", why.description()),
+            Err(why) => println!("{}", why.description()),
         }
         println!("{:?}", data);
-
     }
-
 }
