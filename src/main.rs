e54fabd5632fb95f5dd1725d386798b431db9306
Gregor Tudan <gregor.tudan@cofinpro.de>
2021-10-10 15:22:46 +0200

    added air quality scale

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,20 @@
 //! and writing it to influxdb.
 extern crate hidapi;
 extern crate rumqttc;
+extern crate alloc;
 
 use zytemp::Reading;
 use std::thread;
 use rumqttc::{QoS, MqttOptions, Client, Connection};
+use alloc::fmt;
+use std::fmt::{Display, Formatter};
 
 mod zytemp;
 
 fn main() {
     let (client, mut connection) = connect_client();
 
-    thread::spawn(move || publish_readings(client) );
+    thread::spawn(move || publish_readings(client));
 
     for (i, notification) in connection.iter().enumerate() {
         //println!("{}. Notification {:?}", i, notification)
@@ -37,9 +40,38 @@ fn publish_readings(client: Client) -> ! {
         let mut client_sender = client.clone();
 
         let result = match reading {
-            Reading::CO2(v) => client_sender.publish("wohnzimmer/co2monitor/co2", QoS::AtLeastOnce, false, v.to_string().as_bytes()),
+            Reading::CO2(v) =>
+                client_sender.publish("wohnzimmer/co2monitor/co2", QoS::AtLeastOnce, false, v.to_string().as_bytes())
+                    .and(client_sender.publish("wohnzimmer/co2monitor/air_quality", QoS::AtLeastOnce, false, by_co2(v).to_string().as_bytes())),
             Reading::Temperature(v) => client_sender.publish("wohnzimmer/co2monitor/temperature", QoS::AtLeastOnce, false, v.to_string().as_bytes()),
         };
         result.unwrap();
+    }
+}
+
+#[derive(Debug)]
+enum AirQuality {
+    Excellent,
+    Good,
+    Fair,
+    Inferior,
+    Poor,
+    Unknown,
+}
+
+impl Display for AirQuality {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+fn by_co2(co2level: u16) -> AirQuality {
+    return match co2level {
+        200..=400 => AirQuality::Excellent,
+        401..=600 => AirQuality::Good,
+        601..=800 => AirQuality::Fair,
+        801..=1200 => AirQuality::Inferior,
+        1200..=2000 => AirQuality::Poor,
+        _ => AirQuality::Unknown
     }
 }
